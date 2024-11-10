8ba3c18e94de675480885e63fc667815064c61a9
stasaaa <radojicic.stasa@gmail.com>
2024-11-11 00:59:34 +0100

    tring to add images

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <NavBar></NavBar>
-  <MapComponent/>
   <router-view></router-view>
 </template>
 
 <script>
 import '@coreui/coreui/dist/css/coreui.min.css'
 import NavBar from './components/NavBar.vue'
-import MapComponent from './components/MapComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    NavBar, 
-    MapComponent
+    NavBar
   }
 }
 </script>
---
+++ src/view/CreatePost.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <CFormTextarea
+            id="description"
+            label="Post description"
+            rows="3">
+        </CFormTextarea>
+    </div>
+</template>
+
+<script setup>
+import { CFormTextarea } from '@coreui/vue';
+// import { useStore } from 'vuex';
+
+// const store = useStore();
+
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+}
+</style>
