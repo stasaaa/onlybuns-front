2da54559ff2411d6c278926f6ed9563a514076ee
Ana Micic <anamicic1174@gmail.com>
2025-07-03 02:14:57 +0200

    Page for app analytics added

--- src/router/router.js
+++ src/router/router.js
@@ -1,63 +1,70 @@
-import LandingPage from '@/view/LandingPage.vue';
-import LogIn from '@/view/LogIn.vue';
-import RegisterUser from '@/view/RegisterUser.vue';
-import ActivateAccount from '@/view/ActivateAccount.vue';
-import CreatePost from '@/view/CreatePost.vue';
-import BunnyFeed from '@/view/BunnyFeed.vue';
-import RegisteredUsers from '@/view/RegisteredUsers.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import { useStore } from 'vuex';
-import UserProfile from '@/view/UserProfile.vue';
+import AppAnalytics from "@/components/AppAnalytics.vue";
+import ActivateAccount from "@/view/ActivateAccount.vue";
+import BunnyFeed from "@/view/BunnyFeed.vue";
+import CreatePost from "@/view/CreatePost.vue";
+import LandingPage from "@/view/LandingPage.vue";
+import LogIn from "@/view/LogIn.vue";
+import RegisteredUsers from "@/view/RegisteredUsers.vue";
+import RegisterUser from "@/view/RegisterUser.vue";
+import UserProfile from "@/view/UserProfile.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { useStore } from "vuex";
 
 const routes = [
   {
-    path: '/register',
-    name: 'RegisterUser',
+    path: "/register",
+    name: "RegisterUser",
     component: RegisterUser,
-    meta: { requiresGuest: true }
+    meta: { requiresGuest: true },
   },
   {
-    path: '/',
-    name: 'LandingPage',
+    path: "/",
+    name: "LandingPage",
     component: LandingPage,
   },
   {
-    path: '/login',
-    name: 'LogIn',
+    path: "/login",
+    name: "LogIn",
     component: LogIn,
     meta: { requiresGuest: true },
   },
   {
-    path: '/activate',
-    name: 'ActivateAccount',
+    path: "/activate",
+    name: "ActivateAccount",
     component: ActivateAccount,
   },
   {
-    path: '/createPost',
-    name: 'CreatePost',
+    path: "/createPost",
+    name: "CreatePost",
     component: CreatePost,
     meta: { requiresAuth: true },
   },
   {
-    path: '/feed',
-    name: 'BunnyFeed',
-    component: BunnyFeed
+    path: "/feed",
+    name: "BunnyFeed",
+    component: BunnyFeed,
   },
   {
-    path: '/registered-users',
-    name: 'RegisteredUsers',
+    path: "/registered-users",
+    name: "RegisteredUsers",
     component: RegisteredUsers,
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresAdmin: true },
   },
   {
-    path: '/profile',
-    name: 'UserProfile',
-    component: UserProfile
+    path: "/profile",
+    name: "UserProfile",
+    component: UserProfile,
   },
   {
-    path: '/:catchAll(.*)',
-    redirect: '/'
-  }
+    path: "/analytics",
+    name: "Analytics",
+    component: AppAnalytics,
+    meta: { requiresAuth: true, requiresAdmin: true },
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/",
+  },
 ];
 
 const router = createRouter({
@@ -67,22 +74,22 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const store = useStore();
-
   const isAuthenticated = store.state.user.id !== -1;
+  const isAdmin = store.state.user.userRole === "ADMIN";
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login');
-  }
-  else if (isAuthenticated && to.meta.requiresGuest) {
-    next('/');
+    next("/login");
+  } else if (isAuthenticated && to.meta.requiresGuest) {
+    next("/");
+  } else if (to.meta.requiresAdmin && !isAdmin) {
+    next("/"); // Redirect non-admin users to home
   } else {
-    if (to.name === 'UserProfile') {
+    if (to.name === "UserProfile") {
       if (!to.query.username || to.query.username === "") {
-        next('/');
+        next("/");
         return;
       }
     }
-
     next();
   }
 });
